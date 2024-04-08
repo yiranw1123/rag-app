62cf708aa8feb601eca9fdee31ab08746388d3d5
yiran <yiran_w1123@hotmail.com>
2024-04-07 17:09:10 -0700

    added missing UploadFileForm from last commit

---
+++ react-app/src/UploadFileForm.jsx
@@ -0,0 +1,78 @@
+import React, {useEffect, useRef, useState} from 'react';
+import api from './api';
+import styles from "./App.module.css";
+
+const UploadFileForm = ({data, onFormSubmit}) => {
+  const [selectedKB, setSelectedKB] = useState("");
+  const [files, setFiles] = useState([]);
+  const fileInputRef = useRef();
+
+  const handleMultipleChange = (event)=> {
+    setFiles([...event.target.files]);
+  }
+
+  const resetSelection = () => {
+    setSelectedKB("");
+  }
+
+  const handleMultipleSubmit = async (event) =>{
+    event.preventDefault();
+
+    const filesData = new FormData();
+
+    files.forEach((file, index) => {
+      filesData.append('files', file);
+    });
+
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
+    };
+
+    const kbId = document.getElementById("kb-dropdown").value; 
+    console.log(kbId);
+
+    try{
+      const response =  await api.post(`/knowledgebase/${kbId}/upload/`, filesData, config);
+      if(response.status === 201){
+        console.log('Successfully uploaded files to server');
+      } else {
+        throw new Error(`Unexpected status code: ${response.status} `);
+      }
+    } catch (error) {
+      console.error("Error uploading files: ", error);
+      throw error;
+    } finally{
+      // reset file pointer
+      if(fileInputRef.current){
+        fileInputRef.current.value = '';
+      }
+      onFormSubmit();
+      resetSelection();
+    };
+  }
+  return(
+    <div className="upload-file-form">
+      <form onSubmit={handleMultipleSubmit}>
+        <h2>File Upload</h2>
+        <label htmlFor="kb-dropdown">KnowledgeBase</label>
+        <select className='form-select' value={selectedKB} onChange={e => setSelectedKB(e.target.value)} id='kb-dropdown'>
+          <option value="">Select a KnowledgeBase</option>
+          {data.map((kb) => (
+            <option key={kb.id} value={kb.id}>{`${kb.id}:  ${kb.name}`}</option>
+          ))}
+        </select>
+        <div className='mb-3'>
+          <label htmlFor='fupload' className='form-label'>
+            Upload Files
+          </label>
+          <input type='file' className='form-control' id='fupload' name='fupload' onChange={handleMultipleChange} multiple ref={fileInputRef}></input>
+        </div>
+        <button type="submit" className='btn btn-primary'>Upload</button>
+      </form>
+    </div>
+  );
+};
+
+export default UploadFileForm;
